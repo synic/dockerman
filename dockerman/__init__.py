98cdbdb85369b2c26506f234da31ca9bf4b65aeb
Adam Olsen <arolsen@gmail.com>
2021-04-20 13:57:54 -0600

    more things imported by default

--- dockerman/__init__.py
+++ dockerman/__init__.py
@@ -1,9 +1,30 @@
-__all__ = ['command', 'crun', 'main', 'option', 'run']
+__all__ = [
+    'command',
+    'crun',
+    'file',
+    'log',
+    'logcmd'
+    'help',
+    'info',
+    'warning',
+    'error',
+    'main',
+    'option',
+    'run',
+    'set_default_container',
+]
 
 from . import dockerman
 from .dockerman import (  # noqa
     command,
     crun,
+    file,
+    log,
+    logcmd,
+    help,
+    info,
+    warning,
+    error,
     main,
     option,
     run,
